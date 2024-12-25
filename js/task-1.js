9afb99f478726745c3c06f4854a906ed5580a3dc
ruslana <ruslana.lohvyn@gmail.com>
2024-12-25 19:40:58 +0200

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -2,3 +2,23 @@ const categories = document.querySelectorAll(".item");
 
 console.log("Number of categories:", categories.length);
 
+// const titles = document.querySelectorAll(".item > h2");
+
+// const elements = document.querySelectorAll(".item > ul > li");
+// console.log(elements);
+
+
+
+
+categories.forEach(category => {
+    const title = category.querySelector("h2");
+    
+    console.log("Category:", title.textContent);
+
+    const elements = category.querySelectorAll("ul > li");
+
+    console.log("Number of elements:", elements.length);
+
+});
+
+
